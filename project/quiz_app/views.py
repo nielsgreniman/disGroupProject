c5ecd9dfca39156c1245b699376205c297a45c90
jancaesar <jan.caesar@gmail.com>
2023-05-30 17:32:18 +0200

    Added redirect to quiz page from home page. Added error message when no name.

--- project/quiz_app/views.py
+++ project/quiz_app/views.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask import render_template
+from flask import request, url_for, redirect, session
 from datetime import datetime
 from . import app
 
-@app.route("/")
+app.secret_key = "super secret key"
+
+@app.route("/", methods = ['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    noNameError = False
+    if request.method == 'POST':
+        name = request.form.get('name')
+        decade = request.form.get('decade')
+        if not name:
+            noNameError = True
+        else:
+            session["name"] = name
+            session["decade"] = decade
+            return redirect(url_for('quiz'))
+    return render_template("home.html", error=noNameError)
 
 @app.route("/about/")
 def about():
@@ -27,3 +40,12 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+@app.route('/quiz')
+def quiz():
+    name = session.get("name")
+    decade = session.get("decade")
+    return render_template('quiz.html', name=name, decade=decade)
+
+if __name__ == '__main__':
+    app.run(debug=True)
