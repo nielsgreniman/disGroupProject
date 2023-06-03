46cd3c59fe9896acaa35ee32a0611d8c34d64fab
jancaesar <jan.caesar@gmail.com>
2023-06-03 09:31:51 +0200

    Added schemas

---
+++ project/schema_ins.sql
@@ -0,0 +1,4 @@
+INSERT INTO questions(question,movie_attribute) VALUES ('What year did the movie released','year');
+INSERT INTO questions(question,movie_attribute) VALUES ('Who was the director of the movie','director_name');
+INSERT INTO questions(question,movie_attribute) VALUES ('Name a lead actor from the movie','cast_name');
+INSERT INTO questions(question,movie_attribute) VALUES ('What rank (1-250) has the movie','rank');
