4ac8eb43f3eb10b2feb57550912065d901554c11
nielsgreniman <nielsgreniman@gmail.com>
2023-06-06 21:47:07 +0200

    added End Quiz-button

--- project/quiz_app/__init__.py
+++ project/quiz_app/__init__.py
@@ -9,7 +9,7 @@ app.secret_key = "super secret key"
 
 # set your own database name, user name and password
 #db = "dbname='movieProject' user='postgres' host='localhost' password='ordrup'" # Jan
-db = "dbname='movieProject' user='postgres' host='localhost' password='4545'" # Kristian
-#db = "dbname='movieProject' user='postgres' host='localhost' password='user'" # Niels
+#db = "dbname='movieProject' user='postgres' host='localhost' password='4545'" # Kristian
+db = "dbname='movieProject' user='postgres' host='localhost' password='user'" # Niels
 
 conn = psycopg2.connect(db)
