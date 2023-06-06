a03ab9af37409303f6b73d50e3a1cade23f598d6
Cyber-Kriller <79922436+Cyber-Kriller@users.noreply.github.com>
2023-06-06 11:21:27 +0200

    Added path for Kristian

--- project/quiz_app/__init__.py
+++ project/quiz_app/__init__.py
@@ -9,4 +9,5 @@ app.secret_key = "super secret key"
 
 # set your own database name, user name and password
 db = "dbname='movieProject' user='postgres' host='localhost' password='ordrup'" #potentially wrong password
+#db = "dbname='movieProject' user='postgres' host='localhost' password='4545'" # Kristians
 conn = psycopg2.connect(db)
